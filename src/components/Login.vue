5bc490abe3dd1cfa3d42ca7ad41d7133c4021cee
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-08-16 15:52:57 +0530

    login API integeration completed

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-<img class="logo" alt="Vue logo" src="../assets/logo.png" />
+  <img class="logo" alt="Vue logo" src="../assets/logo.png" />
   <h1>Login</h1>
   <div class="login">
     <input type="email" placeholder="Enter your Email" v-model="email" />
@@ -10,11 +10,12 @@
     />
     <button v-on:click="login()">login</button>
     <p>
-      <router-link to="/sign-up" >Not a memebr? go to Sign-Up</router-link>
+      <router-link to="/sign-up">Not a memebr? go to Sign-Up</router-link>
     </p>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "loginC",
   data() {
@@ -24,9 +25,26 @@ export default {
     };
   },
   methods: {
-    login() {
-      //
+    async login() {
+      // go to documnetation and check filter API
+      // change the use of litralls
+      console.log(this.email, this.password);
+      var result = await axios.get(
+        `http://localhost:3000/users?email=${this.email}&&password=${this.password}`
+      );
+      console.log(result.data);
+
+      if ((result.status == 200) & (result.data.length > 0)) {
+        localStorage.setItem("user-info", JSON.stringify(result.data[0])); // as data in the form of array
+        this.$router.push({ name: "Home" });
+      }
     },
+  },
+  mounted() {
+    let user = localStorage.getItem("user-info");
+    if (user) {
+      this.$router.push({ name: "Home" });
+    }
   },
 };
 </script>
